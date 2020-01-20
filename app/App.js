364313b970324dbdc676fcc1c2d4281e6634e4d2
iamhrayr <iamhrayr@gmail.com>
2020-01-20 22:45:38 +0400

    Android added app icon & splash screen (#2)

--- app/App.js
+++ app/App.js
@@ -1,21 +1,28 @@
 // @flow
-import React, { memo } from 'react';
+import React, { memo, useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import SplashScreen from 'react-native-splash-screen';
 
 import { store, persistor } from 'App/redux/store';
 import AppNavigator from 'App/navigator/AppNavigator';
 import theme from 'App/theme';
 
-const App = (): React$Node => (
-  <ThemeProvider theme={theme}>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <AppNavigator />
-      </PersistGate>
-    </Provider>
-  </ThemeProvider>
-);
+const App = (): React$Node => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <AppNavigator />
+        </PersistGate>
+      </Provider>
+    </ThemeProvider>
+  );
+};
 
 export default memo<{}>(App);
